ea9a9d81dc674ff7544aab318c7895c61d7de7da
inden65 <109307056+inden65@users.noreply.github.com>
2024-12-07 13:25:03 -0600

    cambio version de front, primera parte eliminacion antiguo

--- Front - React App/src/pages/home/logo.tsx
+++
@@ -1,9 +0,0 @@
-function Logo() {
-  document.body.style.backgroundImage                 = "url('fondo.png')";
-  document.body.style.backgroundAttachment            = "fixed";
-  document.body.style.backgroundSize                  = "cover";
-
-  return <></>;
-}
-
-export { Logo };
